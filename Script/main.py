316043a9d85f9b5cc704e2d52180d4b343314085
kuomagari-a11y <ku.omagari@gmail.com>
2026-07-26 15:56:29 +0900

    Update main.py

--- Script/main.py
+++ Script/main.py
@@ -1,78 +1,77 @@
-import keyboard
-from tkinter import *
+from scene import *
 
-ball = {
-    "dirx": 15,
-    "diry": -15,
-    "x": 350,
-    "y": 300,
-    "w": 10,
-}
-bar = {
-    "x": 550,
-    "y": 100,
-    "w": 100,
-}
-count = 0
 
-win = Tk()
-cv = Canvas(win, width=600, height=400)
-cv.pack()
+class PongGame(Scene):
+    def setup(self):
+        self.background_color = 'white'
 
-def draw_ball():
-    cv.create_oval(
-        ball["x"] - ball["w"], ball["y"] - ball["w"],
-        ball["x"] + ball["w"], ball["y"] + ball["w"],
-        fill="green")
+        self.ball = {
+            'x': self.size.w / 2,
+            'y': self.size.h / 2,
+            'dirx': 6,
+            'diry': -6,
+            'r': 10,
+        }
+        self.bar = {
+            'x': self.size.w - 30,
+            'y': self.size.h / 2,
+            'h': 100,
+        }
+        self.count = 0
+        self.game_over = False
 
-def draw_bar():
-    cv.create_rectangle(
-        bar["x"] - 5, bar["y"] - bar["w"],
-        bar["x"] + 5, bar["y"] + bar["w"],
-        fill="black")
+    def update(self):
+        if self.game_over:
+            return
 
-def move_ball():
-    global count
-    bx = ball["x"] + ball["dirx"]
-    by = ball["y"] + ball["diry"]
-    
-    if bx < 0:
-        ball["dirx"] *= -1
-    if by < 0 or by > 400:
-        ball["diry"] = ball["diry"] * (-1)
-    if bx > 550 and (by > bar["y"] - bar["w"] and  by < bar["y"] + bar["w"] ):
-        ball["dirx"] *= -1
-        count += 1
-    if bx > 600:
-        cv.create_text(100, 200, text="YOU LOSE", fill="red", font=("Bold", 20))
-        win.quit()
- 
-    ball["x"] += ball["dirx"]
-    ball["y"] += ball["diry"]
+        b = self.ball
+        bar = self.bar
 
-def move_bar():   
-    if keyboard.is_pressed('up'):
-        bar["y"] -= 10
-    if keyboard.is_pressed('down'):
-        bar["y"] += 10
-    
-    if bar["y"] < 0:
-        bar["y"] = 0
-    if bar["y"] > 400:
-        bar["y"] = 400
+        nx = b['x'] + b['dirx']
+        ny = b['y'] + b['diry']
 
-def draw_score():
-    global count
-    cv.create_text(100, 20, text="Score: {}".format(count*100), fill="black", font=("Arial", 16))
+        if nx - b['r'] < 0:
+            b['dirx'] *= -1
+        if ny - b['r'] < 0 or ny + b['r'] > self.size.h:
+            b['diry'] *= -1
 
-def game_loop():
-    cv.delete("all")
-    draw_ball()
-    draw_bar()
-    move_ball()
-    move_bar()
-    draw_score()
-    win.after(20, game_loop)
+        if (nx + b['r'] > bar['x'] - 5 and
+                bar['y'] - bar['h'] < ny < bar['y'] + bar['h']):
+            b['dirx'] *= -1
+            self.count += 1
 
-game_loop()
-win.mainloop()
+        if nx - b['r'] > self.size.w:
+            self.game_over = True
+
+        b['x'] += b['dirx']
+        b['y'] += b['diry']
+
+    def draw(self):
+        background(1, 1, 1)
+
+        b = self.ball
+        fill(0, 0.6, 0)
+        ellipse(b['x'] - b['r'], b['y'] - b['r'], b['r'] * 2, b['r'] * 2)
+
+        bar = self.bar
+        fill(0, 0, 0)
+        rect(bar['x'] - 5, bar['y'] - bar['h'], 10, bar['h'] * 2)
+
+        fill(0, 0, 0)
+        text('Score: {}'.format(self.count * 100), font_size=24,
+             x=90, y=self.size.h - 30, alignment=5)
+
+        if self.game_over:
+            fill(1, 0, 0)
+            text('YOU LOSE', font_size=40,
+                 x=self.size.w / 2, y=self.size.h / 2, alignment=5)
+
+    def touch_moved(self, touch):
+        self.bar['y'] = touch.location.y
+        if self.bar['y'] - self.bar['h'] < 0:
+            self.bar['y'] = self.bar['h']
+        if self.bar['y'] + self.bar['h'] > self.size.h:
+            self.bar['y'] = self.size.h - self.bar['h']
+
+
+run(PongGame())
